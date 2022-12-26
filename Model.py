29978cc5e386af56f57904f51065eb707db6a1ae
Garance <garance.despreaux@hotmail.fr>
2022-12-26 15:40:05 +0100

    test

--- Model.py
+++ Model.py
@@ -4,3 +4,7 @@ Vasconcelos VV, Santos FC, Pacheco JM, Levin SA. 2014
 Climate policies under wealth inequality. Proc. Natl Acad. Sci. USA 111, 2212-2216.
 (doi:10.1073/pnas.1323479111) Crossref, PubMed, ISI, Google Scholar
 """
+
+Z = 6 #Number of individuals
+Zr = 3 #Number of rich individuals
+Zp = Z - Zr #Number of poor individuals
