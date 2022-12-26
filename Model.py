bf814c43a7cb6e47581a63bf723a4f06823137f9
SynaSpike <ds.okplayer1998@gmail.com>
2022-12-26 11:57:37 +0100

    Initial file

---
+++ Model.py
@@ -0,0 +1,6 @@
+""""
+This file is used to code for the main model of the article: https://www.pnas.org/doi/full/10.1073/pnas.1323479111
+Vasconcelos VV, Santos FC, Pacheco JM, Levin SA. 2014
+Climate policies under wealth inequality. Proc. Natl Acad. Sci. USA 111, 2212-2216.
+(doi:10.1073/pnas.1323479111) Crossref, PubMed, ISI, Google Scholar
+"""
